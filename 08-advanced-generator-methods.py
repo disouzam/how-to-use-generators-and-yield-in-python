b4fbbec2579a640c7dd56383d6c1989dcabacb09
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-11 15:18:21 -0300

    Print statements added to help understand how yield, next and send work together

--- 08-advanced-generator-methods.py
+++ 08-advanced-generator-methods.py
@@ -18,13 +18,20 @@ def infinite_palindromes():
     num = 0
     while True:
         if is_palindrome(num):
+            print(f"{num} is a palindromic number")
             i = (yield num)
+            
             if i is not None:
+                print(f"This is the value send method sent: {i}. num is {num} but it will become {i}")
                 num = i
+            else:
+                print(f"Next calls send no value and them i values: {i} and num stays at: {num}")
         num += 1
 
 
 pal_gen = infinite_palindromes()
 for i in pal_gen:
+    print(f"Next was just called and returned: {i}")
     digits = len(str(i))
+    print(f"Send method will be called with value: {10 ** (digits)}")
     pal_gen.send(10 ** (digits))
