c1dae3a9798408f216fe14c9802a212c8c08fc7e
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-11 15:46:09 -0300

    Removed all print statements from script that process a CSV file in a pipeline

--- 11-data-processing-pipelines.py
+++ 11-data-processing-pipelines.py
@@ -1,17 +1,12 @@
 file_name = "data/techcrunch.csv"
 lines = (line for line in open(file_name))
-print(lines)
 list_line = (s.rstrip().split(",") for s in lines)
-print(list_line)
 cols = next(list_line)
-print(cols)
 company_dicts = (dict(zip(cols, data)) for data in list_line)
-print(company_dicts)
 funding = (
     int(company_dict["raisedAmt"])
     for company_dict in company_dicts
     if company_dict["round"] == "a"
 )
-print(funding)
 total_series_a = sum(funding)
 print(f"Total series A fundraising: ${total_series_a}")
