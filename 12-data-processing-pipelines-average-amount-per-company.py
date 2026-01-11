93072685473035a0cbe3bddc245b48716ee4c9cd
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-11 15:58:43 -0300

    Modification to calculate average amount raised per company without redefining generators

--- 12-data-processing-pipelines-average-amount-per-company.py
+++ 12-data-processing-pipelines-average-amount-per-company.py
@@ -19,7 +19,14 @@ funding = (
     if company_dict["round"] == "a"
 )
 
+funding_materialized = [cf for cf in funding]
+print(funding_materialized)
+
 # Sum the raised amount per company
-total_series_a = sum(funding)
+total_series_a = sum(funding_materialized)
+
+# Average raised amount per company
+average = total_series_a / len(funding_materialized)
 
 print(f"Total series A fundraising: ${total_series_a}")
+print(f"Average amount per company was {average:.2f}")
