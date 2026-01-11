87036e5e35742bd8b817bad7303495cee38f55c5
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-11 11:34:49 -0300

    Reading CSV files using lazy reading - yield statement

---
+++ 02-reading-csv-files-lazy.py
@@ -0,0 +1,11 @@
+def csv_reader(file_name):
+    for row in open(file_name, "r"):
+        yield row
+
+csv_gen = csv_reader("data/techcrunch.csv")
+row_count = 0
+
+for row in csv_gen:
+    row_count += 1
+
+print(f"Row count is {row_count}")
